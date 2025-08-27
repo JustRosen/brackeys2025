89f33745cfe1668563739800adfc0977773145fd
JustRosen <permafrostxz@gmail.com>
2025-08-27 14:27:26 -0400

    more text

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Game:
         pygame.display.set_caption("Brackeys 2025")
         self.clock = pygame.Clock()
         print("hello world")
-        print("smth")
+        print("this is the main branch")
         
         asyncio.run(self.run())
 
